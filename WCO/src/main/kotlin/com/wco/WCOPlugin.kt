34a076ecc2fcd2f985358c0f681263ce61cffafd
YoureMotherHelped-Love-Mom <youremotherhelped-love-mom@users.noreply.github.com>
2026-05-21 12:24:53 +0800

    Remove WCOSettings, fix WCOPlugin to match simplified WCO constructor

--- WCO/src/main/kotlin/com/wco/WCOPlugin.kt
+++ WCO/src/main/kotlin/com/wco/WCOPlugin.kt
@@ -1,27 +1,11 @@
 package com.wco
 
-import android.content.Context
-import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class WCOPlugin : Plugin() {
-    lateinit var api: WCO
-
-    override fun load(context: Context) {
-        val sharedPref = context.getSharedPreferences("wco_prefs", Context.MODE_PRIVATE)
-        val savedDomain = sharedPref.getString("selected_domain", null)
-        api = WCO(savedDomain, sharedPref)
-        registerMainAPI(api)
-
-        val activity = context as? AppCompatActivity
-        openSettings = {
-            val frag = WCOSettings(this, sharedPref)
-            activity?.supportFragmentManager?.let { fm ->
-                frag.show(fm, "WCOSettings")
-            }
-        }
+class WCOPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(WCO())
     }
 }
